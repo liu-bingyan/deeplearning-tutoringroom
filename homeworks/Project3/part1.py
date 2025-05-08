4c7effd122c86d3b0b679eda276266b598093f27
Bingyan Liu <78130918+liu-bingyan@users.noreply.github.com>
2025-05-07 22:21:03 -0500

    Delete homeworks directory

--- homeworks/Project3/part1.py
+++
@@ -1,8 +0,0 @@
-!pip install datasets
-!pip install transformers==4.46.2
-!pip install absl-py nltk rouge-score
-!pip install evaluate
-!pip install bert_score
-!pip install peft
-!pip install tf-keras
-
